915db72f4209544e6f16e8c6b9a32a09f14b82ff
amrbashir <github@amrbashir.me>
2025-05-18 06:09:06 +0300

    feat: prisma + mongodb + docker

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { VersioningType } from "@nestjs/common";
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
+
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
-  await app.listen(process.env.NESTJS_SERVER_PORT ?? 3000);
-}
-bootstrap();
+
+  const port = process.env.NESTJS_SERVER_PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Server running on: http://localhost:${port}/v1`);
+})();
