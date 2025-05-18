2a7f04c8972b5e06153e957713a0c6037650913a
amrbashir <github@amrbashir.me>
2025-05-18 04:49:32 +0300

    chore: env file

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
+  await app.listen(process.env.NESTJS_SERVER_PORT ?? 3000);
 }
 bootstrap();
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  server: {
-    port: 1420,
-    strictPort: true, // strictPort for Tauri
-  },
+  return {
+    plugins: [vue()],
+
+    clearScreen: false, // don't clear the console, for Tauri]
+    server: {
+      port: Number(env.VITE_DEV_PORT),
+      strictPort: true, // for Tauri
+
+      proxy: {
+        "/api": {
+          target: `http://localhost:${env.NESTJS_SERVER_PORT}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
