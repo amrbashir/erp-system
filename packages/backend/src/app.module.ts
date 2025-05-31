0e81fbc5e0ea5411533848480591435455a39d39
amrbashir <github@amrbashir.me>
2025-05-31 05:23:35 +0300

    chore: fmt

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { PrismaModule } from "./prisma/prisma.module";
-import { OrgController } from './org/org.controller';
-import { OrgModule } from './org/org.module';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { OrgController } from "./org/org.controller";
+import { OrgModule } from "./org/org.module";
+import { UserService } from "./user/user.service";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [PrismaModule, OrgModule, UserModule, AuthModule],
