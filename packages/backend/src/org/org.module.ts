ee1cd61816de74ce5142a5343a50976d213d9dd9
amrbashir <github@amrbashir.me>
2025-06-02 14:16:07 +0300

    refactor: batch create org and admin in same query

--- packages/backend/src/org/org.module.ts
+++ packages/backend/src/org/org.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { OrgController } from "./org.controller.ts";
 import { OrgService } from "./org.service.ts";
-import { UserModule } from "../user/user.module.ts";
 
 @Module({
-  imports: [UserModule],
   controllers: [OrgController],
   providers: [OrgService],
 })
--- packages/backend/src/org/org.service.ts
+++ packages/backend/src/org/org.service.ts
@@ -1,16 +1,13 @@
 import { BadRequestException, ConflictException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service.ts";
 import { CreateOrgDto } from "./org.dto.ts";
-import { UserService } from "../user/user.service.ts";
 import { type Organization, UserRole } from "../prisma/generated/client.ts";
 import { isValidSlug, slugify } from "../utils.ts";
+import * as argon2 from "argon2";
 
 @Injectable()
 export class OrgService {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(createOrgDto: CreateOrgDto): Promise<Organization | null> {
     if (createOrgDto.slug && !isValidSlug(createOrgDto.slug)) {
@@ -22,20 +19,18 @@ export class OrgService {
     const org = await this.prisma.organization.findUnique({ where: { slug } });
     if (org) throw new ConflictException("Organization already exists");
 
-    const newOrg = await this.prisma.organization.create({
+    return this.prisma.organization.create({
       data: {
         name: createOrgDto.name,
         slug,
+        users: {
+          create: {
+            username: createOrgDto.username,
+            password: await argon2.hash(createOrgDto.password),
+            role: UserRole.ADMIN,
+          },
+        },
       },
     });
-
-    await this.userService.createUser({
-      username: createOrgDto.username,
-      password: createOrgDto.password,
-      organizationId: newOrg.id,
-      role: UserRole.ADMIN,
-    });
-
-    return newOrg;
   }
 }
