230b1250ad2de34452b88570b0cd4d22037ea095
amrbashir <github@amrbashir.me>
2025-06-04 14:10:56 +0300

    test: add unit tests for OrgController

--- packages/backend/src/org/org.controller.ts
+++ packages/backend/src/org/org.controller.ts
@@ -16,3 +16,38 @@ export class OrgController {
     return { organizationId: org.id };
   }
 }
+
+if (import.meta.vitest) {
+  const { it, expect, describe, beforeEach, afterEach } = import.meta.vitest;
+
+  describe("OrgController", async () => {
+    const { PrismaService } = await import("../prisma/prisma.service");
+
+    let prisma;
+    let service: OrgService;
+    let controller: OrgController;
+
+    const { useRandomDatabase } = await import("../../e2e/utils");
+    const { createDatabase, dropDatabase } = useRandomDatabase();
+
+    beforeEach(async () => {
+      await createDatabase();
+      prisma = new PrismaService();
+      service = new OrgService(prisma);
+      controller = new OrgController(service);
+    });
+
+    afterEach(async () => await dropDatabase());
+
+    it("should create an organization", async () => {
+      const createOrgDto: CreateOrgDto = {
+        name: "Test Org",
+        slug: "test-org",
+        username: "admin",
+        password: "12345678",
+      };
+      const result = await controller.create(createOrgDto);
+      expect(result).toEqual({ organizationId: expect.any(String) });
+    });
+  });
+}
