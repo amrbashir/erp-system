1c1d5852041f95baae9e5f7d44aa920bf4567469
amrbashir <github@amrbashir.me>
2025-06-04 10:00:51 +0300

    fix(test): use swc and reflect-metadata to fix tests

--- packages/backend/vitest.config.ts
+++ packages/backend/vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vitest/config";
 import { loadEnv } from "vite";
+import swc from "unplugin-swc";
 
 export default defineConfig(({ mode }) => ({
+  plugins: [swc.vite()],
   test: {
     env: loadEnv(mode, process.cwd(), ""),
     includeSource: ["src/**/*.ts"],
