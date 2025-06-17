59735326fd70dfdd8039ed6f9c028eaddbeb6a25
amrbashir <github@amrbashir.me>
2025-06-17 17:38:27 +0300

    feat: shadcn ui

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -1,19 +1,136 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Home, Info } from "lucide-react";
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useLocation,
+  type RegisteredRouter,
+  type ValidateLinkOptions,
+} from "@tanstack/react-router";
+import { Fragment } from "react/jsx-runtime";
+
+type Route = {
+  url: ValidateLinkOptions<RegisteredRouter, unknown>["to"];
+  title: string;
+  breadCrumb?: string;
+  icon: any;
+};
+
+const routes: Route[] = [
+  { url: "/", title: "Home", breadCrumb: "/", icon: Home },
+  { url: "/about", title: "About", icon: Info },
+];
+
+const AppSideBar = () => {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenuButton asChild size={"lg"}>
+          <Link to="/">
+            <img src="/favicon.svg" className="size-10"></img>
+            <span>Tech Zone Store</span>
+          </Link>
+        </SidebarMenuButton>
+      </SidebarHeader>
+
+      <SidebarContent className="p-2">
+        <SidebarMenu>
+          {routes.map((route) => (
+            <SidebarMenuItem key={route.url}>
+              <SidebarMenuButton asChild>
+                <Link to={route.url}>
+                  <route.icon />
+                  <span>{route.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+const BreadcrubmsForRoutes = () => {
+  const location = useLocation({ select: (state) => state.pathname });
+  const segments = location.split("/").slice(1);
+
+  const segmentsRoutes = segments
+    .map((_, index) => {
+      const url = `/${segments.slice(0, index + 1).join("/")}`;
+      return routes.find((route) => route.url === url);
+    })
+    .filter((r) => !!r);
+
+  // append home route if needed
+  if (segmentsRoutes.length > 0 && segmentsRoutes[0] !== routes[0]) {
+    segmentsRoutes.unshift(routes[0]);
+  }
+
+  const activeSegment = segmentsRoutes[segmentsRoutes.length - 1];
+  const otherSegments = segmentsRoutes.slice(0, -1);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {otherSegments.map((route) => (
+          <Fragment key={route.url}>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to={route.url}>{route.breadCrumb ? route.breadCrumb : route.title}</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </Fragment>
+        ))}
+
+        <BreadcrumbItem>
+          <BreadcrumbPage>
+            {activeSegment.breadCrumb ? activeSegment.breadCrumb : activeSegment.title}
+          </BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
 
 export const Route = createRootRoute({
-  component: () => (
-    <div className="grid grid-cols-[200px_1fr] w-screen h-screen overflow-hidden *:p-4 *:overflow-auto">
-      <aside className="flex flex-col gap-2 border-r *:p-2 *:hover:bg-gray-100">
-        <Link to="/" activeProps={{ className: "bg-red-100" }}>
-          Home
-        </Link>
-        <Link to="/about" activeProps={{ className: "bg-red-100" }}>
-          About
-        </Link>
-      </aside>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  ),
+  component: () => {
+    const cookies = document.cookie.split("; ");
+    const sidebarState = cookies.find((c) => c.startsWith("sidebar_state="))?.split("=")[1];
+    const defaultOpen = sidebarState === "true";
+
+    return (
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSideBar />
+        <main className="p-2">
+          <div className="h-8 flex gap-2 items-center ">
+            <SidebarTrigger />
+            <BreadcrubmsForRoutes />
+          </div>
+          <Outlet />
+        </main>
+      </SidebarProvider>
+    );
+  },
 });
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,6 +17,12 @@ export default defineConfig(({ mode }) => {
       react(),
       tailwindcss(),
     ],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
 
     clearScreen: false, // don't clear the console, for Tauri]
     server: {
