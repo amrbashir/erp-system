82da658e2027a7a02e689a08495dcd6a1b3cf1ca
amrbashir <github@amrbashir.me>
2025-06-20 05:06:13 +0300

    refactor(sdk): use openapi-zod-client generator instead of typed-openapi for better results

--- packages/sdk/src/schema.zod.ts
+++ packages/sdk/src/schema.zod.ts
@@ -1,63 +1,56 @@
 import { z } from "zod";
 
-export type CreateOrgDto = z.infer<typeof CreateOrgDto>;
-export const CreateOrgDto = z.object({
-  name: z.string(),
-  slug: z.union([z.string(), z.undefined()]).optional(),
-  username: z.string(),
-  password: z.string(),
-});
+export const CreateOrgDto = z
+  .object({
+    name: z.string(),
+    slug: z.string().optional(),
+    username: z.string(),
+    password: z.string().min(8),
+  })
+  .passthrough();
 
-export type CreateUserDto = z.infer<typeof CreateUserDto>;
-export const CreateUserDto = z.object({
-  username: z.string(),
-  password: z.string(),
-  organization: z.string(),
-});
+export const CreateUserDto = z
+  .object({ username: z.string(), password: z.string().min(8), organization: z.string() })
+  .passthrough();
 
-export type UserEntity = z.infer<typeof UserEntity>;
-export const UserEntity = z.object({
-  username: z.string(),
-  role: z.union([z.literal("USER"), z.literal("ADMIN")]),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  deletedAt: z.union([z.string(), z.undefined()]).optional(),
-});
+export const UserEntity = z
+  .object({
+    username: z.string(),
+    role: z.enum(["USER", "ADMIN"]),
+    createdAt: z.string().datetime({ offset: true }),
+    updatedAt: z.string().datetime({ offset: true }),
+    deletedAt: z.string().datetime({ offset: true }).optional(),
+  })
+  .passthrough();
 
-export type LoginUserDto = z.infer<typeof LoginUserDto>;
-export const LoginUserDto = z.object({
-  username: z.string(),
-  password: z.string(),
-  organization: z.string(),
-});
+export const LoginUserDto = z
+  .object({ username: z.string(), password: z.string().min(8), organization: z.string() })
+  .passthrough();
 
-export type LoginResponseDto = z.infer<typeof LoginResponseDto>;
-export const LoginResponseDto = z.object({
-  username: z.string(),
-  accessToken: z.string(),
-});
+export const LoginResponseDto = z
+  .object({ username: z.string(), accessToken: z.string() })
+  .passthrough();
 
-export type RefreshTokenResponseDto = z.infer<typeof RefreshTokenResponseDto>;
-export const RefreshTokenResponseDto = z.object({
-  accessToken: z.string(),
-});
+export const RefreshTokenResponseDto = z.object({ accessToken: z.string() }).passthrough();
 
-export type CreateCustomerDto = z.infer<typeof CreateCustomerDto>;
-export const CreateCustomerDto = z.object({
-  name: z.string(),
-  email: z.union([z.string(), z.undefined()]).optional(),
-  phone: z.union([z.string(), z.undefined()]).optional(),
-  organization: z.string(),
-});
+export const CreateCustomerDto = z
+  .object({
+    name: z.string(),
+    email: z.string().optional(),
+    phone: z.string().optional(),
+    organization: z.string(),
+  })
+  .passthrough();
 
-export type CustomerEntity = z.infer<typeof CustomerEntity>;
-export const CustomerEntity = z.object({
-  name: z.string(),
-  email: z.union([z.string(), z.undefined()]).optional(),
-  phone: z.union([z.string(), z.undefined()]).optional(),
-  id: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  deletedAt: z.union([z.string(), z.undefined()]).optional(),
-  organizationId: z.string(),
-});
+export const CustomerEntity = z
+  .object({
+    name: z.string(),
+    email: z.string().optional(),
+    phone: z.string().optional(),
+    id: z.string(),
+    createdAt: z.string().datetime({ offset: true }),
+    updatedAt: z.string().datetime({ offset: true }),
+    deletedAt: z.string().datetime({ offset: true }).optional(),
+    organizationId: z.string(),
+  })
+  .passthrough();
