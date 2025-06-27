5345ff5497bcb58b5d2c07138d0e680a180541e5
amrbashir <github@amrbashir.me>
2025-06-27 04:06:38 +0300

    feat: add caching for i18n

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # erp-system
 
 Building an ERP system for a local store in public.
+
+## Archeticture
+
+### Frontend
+
+- **Framework**: React.js with TypeScript
+- **UI Components**: Shadcn UI and Tailwind CSS
+
+### Backend
+
+- **API**: Node.js with NestJS
+- **Database**: PostgreSQL for relational data
+- **Authentication**: JWT-based auth system
+
+### Infrastructure
+
+- **Deployment**: AWS
+- **CI/CD**: GitHub Actions
--- packages/web/src/i18n.ts
+++ packages/web/src/i18n.ts
@@ -1,12 +1,27 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import ChainedBackend from "i18next-chained-backend";
+import HttpBackend from "i18next-http-backend";
+import LocalStorageBackend from "i18next-localstorage-backend";
 import { initReactI18next } from "react-i18next";
 
 await i18n
-  .use(Backend)
+  .use(ChainedBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({ fallbackLng: ["en", "ar"] });
+  .init({
+    fallbackLng: ["en", "ar"],
+    backend: {
+      backends: [LocalStorageBackend, HttpBackend],
+      backendOptions: [
+        {
+          expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+        },
+        {
+          loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+      ],
+    },
+  });
 
 export default i18n;
