e8536ddbcac585c7ffcd66c26a49870171d1aabf
amrbashir <github@amrbashir.me>
2025-07-01 05:33:50 +0300

    fix(empty-table): replace PackageOpenIcon with GhostIcon

--- packages/web/src/components/empty-table.tsx
+++ packages/web/src/components/empty-table.tsx
@@ -1,4 +1,4 @@
-import { PackageOpenIcon } from "lucide-react";
+import { GhostIcon } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
 export function EmptyTable() {
@@ -6,7 +6,7 @@ export function EmptyTable() {
 
   return (
     <div className="w-full h-full rounded-lg border flex flex-col items-center justify-center">
-      <PackageOpenIcon className="text-muted size-[20%]" />
+      <GhostIcon className="text-muted-foreground/50 size-[20%]" />
       <span className="text-muted-foreground">{t("noDataFound")}</span>
       <span className="text-muted-foreground">{t("noDataFoundDescription")}</span>
     </div>
