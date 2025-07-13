fe494af22d17b88bb139647493723c706bb378f8
amrbashir <github@amrbashir.me>
2025-07-13 08:28:26 +0300

    feat: customer selector drawer on mobile devices

--- packages/web/src/components/invoice-customer-selector.tsx
+++ packages/web/src/components/invoice-customer-selector.tsx
@@ -11,6 +11,7 @@ import {
   CommandItem,
   CommandList,
 } from "@/shadcn/components/ui/command";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/shadcn/components/ui/drawer";
 import { Label } from "@/shadcn/components/ui/label";
 import { PopoverContent, PopoverTrigger } from "@/shadcn/components/ui/popover";
 import { cn } from "@/shadcn/lib/utils";
@@ -18,6 +19,8 @@ import { cn } from "@/shadcn/lib/utils";
 import type { CustomerEntity } from "@erp-system/sdk/zod";
 import type { AnyFieldApi } from "@tanstack/react-form";
 import type z from "zod";
+
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 import { AddCustomerDialog } from "./add-customer-dialog";
 
@@ -35,6 +38,7 @@ export function CustomerSelector({
   const [open, setOpen] = useState(false);
   const [selectedCustomer, setSelectedCustomer] = useState<string | undefined>(undefined);
   const [customerSearch, setCustomerSearch] = useState("");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   function handleCustomerCreated(customer: Customer) {
     field.handleChange(customer.id);
@@ -48,64 +52,79 @@ export function CustomerSelector({
     setOpen(false);
   }
 
+  const ButtonTrigger = (
+    <Button
+      variant="outline"
+      role="combobox"
+      aria-expanded={open}
+      className="flex-1 sm:w-sm justify-between"
+    >
+      {field.state.value
+        ? customers?.find((c) => c.id === field.state.value)?.name
+        : t("customer.select")}
+      <ChevronsUpDownIcon className="opacity-50" />
+    </Button>
+  );
+
+  const CustomersCommanList = (
+    <Command value={selectedCustomer} onValueChange={setSelectedCustomer}>
+      <div className="flex items-center *:first:flex-1 gap-2 p-1 *:data-[slot=command-input-wrapper]:px-0 *:data-[slot=command-input-wrapper]:ps-2">
+        <CommandInput
+          placeholder={t("customer.search")}
+          value={customerSearch}
+          onValueChange={(v) => setCustomerSearch(v)}
+        />
+        <AddCustomerDialog
+          initialName={customerSearch}
+          onCreated={handleCustomerCreated}
+          trigger={
+            <Button variant="ghost" size="icon">
+              <PlusIcon />
+            </Button>
+          }
+        />
+      </div>
+      <CommandList>
+        <CommandEmpty>{t("customer.nomatches")}</CommandEmpty>
+        <CommandGroup>
+          {customers.map((customer) => (
+            <CommandItem
+              key={customer.id}
+              value={customer.name}
+              onSelect={() => handleCustomerSelect(customer)}
+            >
+              {customer.name}
+              <CheckIcon
+                className={cn(
+                  "ml-auto",
+                  field.state.value === customer.id ? "opacity-100" : "opacity-0",
+                )}
+              />
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+
+  if (isDesktop) {
+    return (
+      <div className="flex gap-2">
+        <Label htmlFor={field.name}>{t("customer.name")}</Label>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>{ButtonTrigger}</PopoverTrigger>
+          <PopoverContent className="w-[calc(100vw-1rem)] translate-x-[0.5rem]! sm:w-sm sm:translate-x-0! p-0">
+            {CustomersCommanList}
+          </PopoverContent>
+        </Popover>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex gap-2">
-      <Label htmlFor={field.name}>{t("customer.name")}</Label>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="flex-1 sm:w-sm justify-between"
-          >
-            {field.state.value
-              ? customers?.find((c) => c.id === field.state.value)?.name
-              : t("customer.select")}
-            <ChevronsUpDownIcon className="opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[calc(100vw-1rem)] translate-x-[0.5rem]! sm:w-sm sm:translate-x-0! p-0">
-          <Command value={selectedCustomer} onValueChange={setSelectedCustomer}>
-            <div className="flex items-center *:first:flex-1 gap-2 p-1 *:data-[slot=command-input-wrapper]:px-0 *:data-[slot=command-input-wrapper]:ps-2">
-              <CommandInput
-                placeholder={t("customer.search")}
-                value={customerSearch}
-                onValueChange={(v) => setCustomerSearch(v)}
-              />
-              <AddCustomerDialog
-                initialName={customerSearch}
-                onCreated={handleCustomerCreated}
-                trigger={
-                  <Button variant="ghost" size="icon">
-                    <PlusIcon />
-                  </Button>
-                }
-              />
-            </div>
-            <CommandList>
-              <CommandEmpty>{t("customer.nomatches")}</CommandEmpty>
-              <CommandGroup>
-                {customers.map((customer) => (
-                  <CommandItem
-                    key={customer.id}
-                    value={customer.name}
-                    onSelect={() => handleCustomerSelect(customer)}
-                  >
-                    {customer.name}
-                    <CheckIcon
-                      className={cn(
-                        "ml-auto",
-                        field.state.value === customer.id ? "opacity-100" : "opacity-0",
-                      )}
-                    />
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    </div>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{ButtonTrigger}</DrawerTrigger>
+      <DrawerContent className="bg-popover">{CustomersCommanList}</DrawerContent>
+    </Drawer>
   );
 }
