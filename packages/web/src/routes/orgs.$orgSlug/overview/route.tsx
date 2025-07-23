a3c48c1447bd6bfdebd946e812bb831c4b4baad1
amrbashir <github@amrbashir.me>
2025-07-23 06:27:48 +0300

    fix: adjust grid layout for organization overview

--- packages/web/src/routes/orgs.$orgSlug/overview/route.tsx
+++ packages/web/src/routes/orgs.$orgSlug/overview/route.tsx
@@ -53,7 +53,7 @@ function RouteComponent() {
 
   return (
     <div className="flex flex-col gap-4 p-4">
-      <div className="*:data-[slot=card]:from-white/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+      <div className="*:data-[slot=card]:from-white/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid sm:grid-cols-2 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
         <Card>
           <CardHeader>
             <CardDescription>{t("org.name")}</CardDescription>
