efe8dbb6c1e3ce370171edf1c2c9cc6803ec52ce
amrbashir <github@amrbashir.me>
2025-08-07 06:19:03 +0300

    feat: serve SPA assets from server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,3 +1,4 @@
+import { serveDir, serveFile } from "@std/http/file-server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { handleHealthCheck } from "./health/index.ts";
@@ -9,14 +10,22 @@ import { appRouter } from "./trpc/router.ts";
 
 export default {
   async fetch(req) {
+    // Serve static files from the ../web/dist directory
+    const response = await serveDir(req, { fsRoot: "../web/dist" });
+
+    // If the requested file is not found, serve index.html for SPA routing
+    if (response.status === 404) {
+      return await serveFile(req, "./dist/index.html");
+    }
+
     const { pathname } = new URL(req.url);
 
-    if (pathname.startsWith("/health")) {
+    if (pathname.startsWith("/api/health")) {
       return handleHealthCheck();
     }
 
     return await fetchRequestHandler({
-      endpoint: "/trpc",
+      endpoint: "/api/trpc",
       req,
       router: appRouter,
       createContext,
