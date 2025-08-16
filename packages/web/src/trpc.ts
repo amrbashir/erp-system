45c18aa372d8c28e79d16bcbcb25d584c0380d64
amrbashir <github@amrbashir.me>
2025-08-16 21:15:10 +0300

    feat: add placeholder data handling to query client configuration

--- packages/web/src/trpc.ts
+++ packages/web/src/trpc.ts
@@ -1,11 +1,17 @@
-import { QueryClient } from "@tanstack/react-query";
+import { keepPreviousData, QueryClient } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import superjson from "superjson";
 
 import type { AppRouter } from "@erp-system/server/trpc/router.ts"; // Importing only the type for the AppRouter
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      placeholderData: keepPreviousData,
+    },
+  },
+});
 
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [
