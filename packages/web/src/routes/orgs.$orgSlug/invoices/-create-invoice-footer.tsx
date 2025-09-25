e7130d906a4ad62238d3cc65b46214d96e48ceae
amrbashir <github@amrbashir.me>
2025-09-25 18:05:51 +0300

    feat: improve creating invoice footer layout

--- packages/web/src/routes/orgs.$orgSlug/invoices/-create-invoice-footer.tsx
+++ packages/web/src/routes/orgs.$orgSlug/invoices/-create-invoice-footer.tsx
@@ -2,7 +2,7 @@ import { useStore } from "@tanstack/react-form";
 import { ChevronUpIcon } from "lucide-react";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { Card, CardContent } from "@/shadcn/components/ui/card.tsx";
+import { Card } from "@/shadcn/components/ui/card.tsx";
 import {
   Drawer,
   DrawerContent,
@@ -11,10 +11,8 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/shadcn/components/ui/drawer.tsx";
-import { Label } from "@/shadcn/components/ui/label.tsx";
-import { Separator } from "@/shadcn/components/ui/separator.tsx";
+import { Table, TableBody, TableCell, TableRow } from "@/shadcn/components/ui/table.tsx";
 import { useIsMobile } from "@/shadcn/hooks/use-mobile.ts";
-import { cn } from "@/shadcn/lib/utils.ts";
 
 import type { InvoiceType } from "@erp-system/server/prisma/index.ts";
 import type { ReactFormExtendedApi } from "@tanstack/react-form";
@@ -22,7 +20,6 @@ import type { ReactFormExtendedApi } from "@tanstack/react-form";
 import { InputNumpad } from "@/components/ui/input-numpad.tsx";
 import { formatMoney } from "@/utils/formatMoney.ts";
 import {
-  calculateInvoicePercentDiscount,
   calculateInvoiceRemaining,
   calculateInvoiceSubtotal,
   calculateInvoiceTotal,
@@ -47,7 +44,6 @@ type Invoice = {
 export function InvoiceFooter<TInvoice extends Invoice>({
   invoiceType,
   form,
-  ...props
 }: {
   invoiceType: InvoiceType;
   // deno-lint-ignore no-explicit-any
@@ -68,98 +64,94 @@ export function InvoiceFooter<TInvoice extends Invoice>({
     [invoiceItems],
   );
 
-  const percentDiscount = calculateInvoicePercentDiscount(subtotal, discountPercent);
-  const totalPrice = calculateInvoiceTotal(subtotal, discountPercent, discountAmount);
-  const remainingAmount = calculateInvoiceRemaining(totalPrice, paid);
+  const total = calculateInvoiceTotal(subtotal, discountPercent, discountAmount);
+  const remaining = calculateInvoiceRemaining(total, paid);
 
   const FooterCard = (
-    <Card className={cn("md:w-fit md:ms-auto gap-2 p-2", isMobile && "border-none")} {...props}>
-      <CardContent className="grid grid-cols-[auto_1fr_auto] items-center gap-2 p-2">
-        <Label className="font-semibold">{t("common.form.subtotal")}:</Label>
-        <span></span>
-        <span className="text-end font-semibold">{formatMoney(subtotal)}</span>
-
-        <form.Field
-          name="discountPercent"
-          children={(field) => (
-            <>
-              <Label htmlFor={field.name}>{t("common.form.discountPercent")}:</Label>
-              <InputNumpad
-                name={field.name}
-                className="w-20"
-                value={field.state.value as number}
-                // TODO: Fix type
-                // deno-lint-ignore no-explicit-any
-                onChange={(e) => field.handleChange(e.target.valueAsNumber as any)}
-                min={0}
-                max={100}
+    <Card className="p-0 rounded-none">
+      <Table>
+        <TableBody className="border *:*:not-last:border-e *:*:odd:bg-muted *:*:odd:font-bold *:*:even:w-full">
+          <TableRow>
+            <TableCell>{t("common.form.subtotal")}</TableCell>
+            <TableCell>{formatMoney(subtotal)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>{t("common.form.discountAmount")}</TableCell>
+            <TableCell className="p-0">
+              <form.Field
+                name="discountAmount"
+                children={(field) => (
+                  <InputNumpad
+                    variant="ghost"
+                    name={field.name}
+                    value={new SafeDecimal((field.state.value as string) || 0).toNumber()}
+                    // TODO: Fix type
+                    // deno-lint-ignore no-explicit-any
+                    onChange={(e) => field.handleChange(e.target.value as any)}
+                    min={0}
+                    max={subtotal.toNumber()}
+                  />
+                )}
               />
-            </>
-          )}
-        />
-        <span className="text-end">-{formatMoney(percentDiscount)}</span>
-
-        <form.Field
-          name="discountAmount"
-          children={(field) => (
-            <>
-              <Label htmlFor={field.name}>{t("common.form.discountAmount")}:</Label>
-              <InputNumpad
-                name={field.name}
-                className="w-20"
-                value={new SafeDecimal((field.state.value as string) || 0).toNumber()}
-                // TODO: Fix type
-                // deno-lint-ignore no-explicit-any
-                onChange={(e) => field.handleChange(e.target.value as any)}
-                min={0}
-                max={subtotal.toNumber()}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>{t("common.form.discountPercent")}</TableCell>
+            <TableCell className="p-0">
+              <form.Field
+                name="discountPercent"
+                children={(field) => (
+                  <InputNumpad
+                    variant="ghost"
+                    name={field.name}
+                    value={field.state.value as number}
+                    // TODO: Fix type
+                    // deno-lint-ignore no-explicit-any
+                    onChange={(e) => field.handleChange(e.target.valueAsNumber as any)}
+                    min={0}
+                    max={100}
+                  />
+                )}
               />
-            </>
-          )}
-        />
-        <span className="text-end">-{formatMoney(discountAmount || 0)}</span>
-
-        <Separator className="col-span-3 my-1" />
-
-        <Label className="font-bold">{t("common.form.total")}:</Label>
-        <span></span>
-        <span className="text-end font-bold">{formatMoney(totalPrice)}</span>
-
-        <form.Field
-          name="paid"
-          children={(field) => (
-            <>
-              <Label htmlFor={field.name}>{t("common.form.paid")}:</Label>
-              <InputNumpad
-                name={field.name}
-                className="w-20"
-                value={new SafeDecimal((field.state.value as string) || 0).toNumber()}
-                // TODO: Fix type
-                // deno-lint-ignore no-explicit-any
-                onChange={(e) => field.handleChange(e.target.value as any)}
-                min={0}
-                max={totalPrice.toNumber()}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>{t("common.form.total")}</TableCell>
+            <TableCell>{formatMoney(total)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>{t("common.form.paid")}</TableCell>
+            <TableCell className="p-0">
+              <form.Field
+                name="paid"
+                children={(field) => (
+                  <InputNumpad
+                    className={
+                      invoiceType === "SALE"
+                        ? "text-green-500 dark:text-green-300"
+                        : "text-red-500 dark:text-red-300"
+                    }
+                    variant="ghost"
+                    name={field.name}
+                    value={new SafeDecimal((field.state.value as string) || 0).toNumber()}
+                    // TODO: Fix type
+                    // deno-lint-ignore no-explicit-any
+                    onChange={(e) => field.handleChange(e.target.value as any)}
+                    min={0}
+                    max={total.toNumber()}
+                  />
+                )}
               />
-            </>
-          )}
-        />
-        <span
-          className={cn(
-            "text-end",
-            invoiceType === "SALE"
-              ? "text-green-500 dark:text-green-300"
-              : "text-red-500 dark:text-red-300",
-          )}
-        >
-          {formatMoney(paid || 0)}
-        </span>
-
-        <Label>{t("common.form.remaining")}:</Label>
-        <span></span>
-        <span className="text-end text-red-500 dark:text-red-300">
-          {formatMoney(remainingAmount)}
-        </span>
-      </CardContent>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>{t("common.form.remaining")}</TableCell>
+            <TableCell className="text-red-500 dark:text-red-300">
+              {formatMoney(remaining)}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </Card>
   );
 
